c7d1665aa7171e46304c51e88c30f07fbf979ecb
Auguste MANOU <email@email.com>
2022-01-13 00:20:05 +0100

    Add and configure vuetify

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+export default {
+    head: {
+        title: 'Suivi ICC',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        ],
+    },
+    buildModules: [
+        // Simple usage
+        '@nuxtjs/vuetify',
+
+        // With options
+        ['@nuxtjs/vuetify', { /* module options */ }]
+    ]
+}
