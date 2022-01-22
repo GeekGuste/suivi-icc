d5b90a85032bb8f6c833c3642e41037c02a6ed1d
Auguste MANOU <email@email.com>
2022-01-23 00:43:53 +0100

    gestion de connexion/déconnexion

---
+++ suivi-icc-front/layouts/admin.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- Other stuff -->
+   <div class="authentication-buttons">
+      <div v-if="$auth.loggedIn">
+         {{ $auth.user.email }}
+         <v-btn @click="$auth.logout()" to="/logout" class="logout-btn">
+           Se déconnecter 
+         </v-btn>
+      </div>
+      <div v-else>
+         <v-btn icon to="/login" class="login-btn">
+            <v-icon>mdi-login</v-icon>
+         </v-btn>
+         <v-btn icon to="/register" class="register-btn">
+            <v-icon>mdi-account-key-outline</v-icon>
+         </v-btn>
+      </div>
+   </div>
+<!-- Other stuff -->
+</template>
+
+<script>
+export default {
+  // Some more stuff
+}
+</script>
